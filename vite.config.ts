4bbb210dbfd27b9ab11f497ad237a20107e59e9e
Erol Aslan <erolaslan@msn.com>
2024-10-28 23:20:59 +0300

    Frontend: Fix2

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,       // Uygulamanın 3000 portunda çalışmasını sağlar
+    host: true,       // Dış IP adreslerinden erişilebilir yapar
+    open: true        // Uygulamayı başlattığınızda tarayıcıyı otomatik açar (isteğe bağlı)
+  },
+  preview: {
+    port: 3000,       // Vite 'preview' modunda da 3000 portunu kullanır
+    host: true        // Dış IP adreslerinden erişilebilir yapar
+  }
 })
