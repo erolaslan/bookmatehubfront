4be2151ff629e061bab348aacbb6201a003416d5
Erol Aslan <erolaslan@msn.com>
2024-10-29 12:10:49 +0300

    Frontend: Fix10

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,6 +10,6 @@ export default defineConfig({
     hmr: {
       protocol: 'wss', // WebSocket üzerinden SSL
       host: 'www.bookmatehub.com', // Ana alan adınız
-    }
-  }
+    },
+  },
 });
