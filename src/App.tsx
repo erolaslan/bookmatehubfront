5b8364a3769ae3059d2713d41053dc6f1281faa7
Erol Aslan <erolaslan@msn.com>
2024-10-29 11:52:45 +0300

    Frontend: Fix10

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import BookList from "./pages/BookList";
 import AddBook from "./pages/AddBook";
 
 const App: React.FC = () => (
-    <BrowserRouter>
-        <Routes>
-            {/* /api yollarını yok sayacak şekilde yönlendirin */}
-            <Route path="/api/*" element={null} />
+    <Routes>
+        {/* /api yollarını yok sayacak şekilde yönlendirin */}
+        <Route path="/api/*" element={<Navigate replace to="/" />} />
 
-            {/* Diğer rotalar */}
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/books" element={<BookList />} />
-            <Route 
-                path="/books/create" 
-                element={<AddBook onBookAdded={() => {}} onClose={() => {}} />} 
-            />
-        </Routes>
-    </BrowserRouter>
+        {/* Diğer rotalar */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/books" element={<BookList />} />
+        <Route 
+            path="/books/create" 
+            element={<AddBook onBookAdded={() => {}} onClose={() => {}} />} 
+        />
+    </Routes>
 );
 
 export default App;
