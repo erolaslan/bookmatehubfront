c80313c52acfce996f38f8909ff950af15fa832a
Erol Aslan <erolaslan@msn.com>
2024-10-29 11:44:22 +0300

    Frontend: Fix10

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import BookList from "./pages/BookList";
 import AddBook from "./pages/AddBook";
 
 const App: React.FC = () => (
-    <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/books" element={<BookList />} />
-        
-        {/* AddBook bileşeni için gerekli propsları ekleyin */}
-        <Route 
-            path="/books/create" 
-            element={<AddBook onBookAdded={() => {}} onClose={() => {}} />} 
-        />
-    </Routes>
+    <BrowserRouter>
+        <Routes>
+            {/* /api yollarını yok sayacak şekilde yönlendirin */}
+            <Route path="/api/*" element={null} />
+
+            {/* Diğer rotalar */}
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/books" element={<BookList />} />
+            <Route 
+                path="/books/create" 
+                element={<AddBook onBookAdded={() => {}} onClose={() => {}} />} 
+            />
+        </Routes>
+    </BrowserRouter>
 );
 
 export default App;
--- src/utils/authProvider.ts
+++ src/utils/authProvider.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const login = async ({ username, password }: { username: string; password: string }) => {
-    const response = await axios.post("http://localhost:5172/api/auth/login", {
+    const response = await axios.post("https://bookmatehub/api/auth/login", {
         username,
         password,
     });
