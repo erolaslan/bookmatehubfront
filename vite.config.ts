753d725c24a86782c4c07e12d24a30a5614115d7
Erol Aslan <erolaslan@msn.com>
2024-10-29 09:47:13 +0300

    Frontend: Fix8

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,               // Uygulamanın 3000 portunda çalışmasını sağlar
-    host: true,               // Dış IP adreslerinden erişilebilir yapar
+    port: 3000,
+    host: true,
+    open: true,
     hmr: {
-      protocol: 'wss',        // WebSocket üzerinden SSL kullanır
-      host: 'bookmatehub.com' // Uygulamanın çalıştığı ana bilgisayar adı
+      protocol: 'wss', // WebSocket üzerinden SSL
+      host: 'www.bookmatehub.com', // Ana alan adınız
     }
-  },
-  preview: {
-    port: 3000,               // Vite 'preview' modunda da 3000 portunu kullanır
-    host: true                // Dış IP adreslerinden erişilebilir yapar
   }
-})
+});
